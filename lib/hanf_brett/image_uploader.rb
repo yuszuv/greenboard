a4d1bcdf166bc0206b5cc61e7e608f74e4aea948
yuszuv <jan@sternprodukt.de>
2021-05-17 15:43:45 +0200

    add validation to image uploader

--- lib/hanf_brett/image_uploader.rb
+++ lib/hanf_brett/image_uploader.rb
@@ -5,12 +5,14 @@ require "image_processing/mini_magick"
 Shrine.plugin :validation
 Shrine.plugin :validation_helpers
 Shrine.plugin :derivatives
+Shrine.plugin :store_dimensions
 
 module HanfBrett
   class ImageUploader < Shrine
     Attacher.validate do
+      validate_size      1..5*1024*1024
+      validate_mime_type %w[image/jpeg image/png image/webp image/tiff]
       validate_extension %w[jpg jpeg png webp tiff tif]
-      # ... perform validation ... 
     end
 
     Attacher.derivatives do |original|
@@ -20,7 +22,8 @@ module HanfBrett
         large:  magick.resize_to_limit!(800, 800),
         medium: magick.resize_to_limit!(500, 500),
         small:  magick.resize_to_limit!(300, 300),
-        thumbnail:  magick.resize_to_fill!(200, 200),
+        # thumbnail:  magick.resize_to_fill!(200, 200),
+        thumbnail:  magick.resize_to_limit!(nil, 200),
       }
     end
 
