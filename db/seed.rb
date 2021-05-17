51e27443fa88f82ea500e45f3ce7bff5af64e5a3
yuszuv <jan@sternprodukt.de>
2021-05-17 23:29:33 +0200

    clean up seeds

--- db/seed.rb
+++ db/seed.rb
@@ -6,24 +6,4 @@ require_relative '../spec/support/db/factory'
 
 Hanami.application.boot
 
-10.times { Factory[:card] }
-
-# main_container = Main::Container
-
-# card_repo = main_container['repositories.card']
-
-# card_repo.create({
-#   type: 'SUCHE',
-#   author: 'foo',
-#   topic: 'bar',
-#   text: 'lorem ipsum',
-#   password: 'secret123'
-# })
-
-# card_repo.create({
-#   type: 'BIETE',
-#   author: 'foo',
-#   topic: 'bar',
-#   text: 'lorem ipsum',
-#   password: 'secret123'
-# })
+3.times { Factory[:card, :with_photos] }
