0122d4aad40f9efaed4ff16c01f7f547ed7ef3ae
yuszuv <jan@sternprodukt.de>
2021-10-04 23:26:38 +0200

    add captca to show contact data

---
+++ slices/main/assets/public/ContactToggler.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <p class="card-text" v-if="!clicked">
+      <a href="#" v-on:click="toggleClicked">
+        Kontaktdaten anzeigen
+      </a>
+    </p>
+    <template v-else>
+      <div>
+        <p v-if="robot" class="card-text">
+          {{ contact }}
+        </p>
+        <vue-recaptcha v-else
+                      ref="recaptcha"
+                      @verify="onVerify"
+                      sitekey="6LdVv6scAAAAAMHE9Qv0PJBWYNqUApXymde2q3q-"
+                      :loadRecaptchaScript="true">
+        </vue-recaptcha>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import VueRecaptcha from 'vue-recaptcha'
+
+export default {
+  data() {
+    return {
+      robot: false,
+      clicked: false,
+      contact: undefined,
+    }
+  },
+  props: {
+    cardId: {
+      type: Number,
+      required: true,
+    }
+  },
+  components: {
+    VueRecaptcha
+  },
+  methods: {
+    onVerify(response) {
+      if (response) this.robot = true;
+
+      fetch(`karte/${this.cardId}/kontaktdaten`, { headers: {
+        'Content-Type': 'application/json'
+      }})
+        .then(response => response.json())
+        .then(data => this.contact = data)
+
+
+    },
+    toggleClicked(event) {
+      event.preventDefault()
+      this.clicked = !this.clicked
+    },
+  }
+}
+</script>
--- slices/main/assets/public/index.js
+++ slices/main/assets/public/index.js
@@ -7,12 +7,23 @@ import "./photoswipe";
 
 import Vue from "vue";
 import App from "./App.vue";
+import ContactToggler from "./ContactToggler.vue";
 
 document.addEventListener("DOMContentLoaded", function() {
   if (document.getElementById("app")) {
     new Vue({
       el: "#app",
       render: h => h(App)
+    });
+  }
+
+  const togglers = document.getElementsByClassName("contact-toggler");
+  for (var i = 0; i < togglers.length; i++) {
+    const context = { props: { cardId: parseInt(togglers[i].dataset.id) } };
+
+    new Vue({
+      el: togglers[i],
+      render: h => h(ContactToggler, context)
     });
   }
 });
