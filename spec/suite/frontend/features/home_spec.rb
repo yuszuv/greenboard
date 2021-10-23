9fe4d53a4e5961c77c9b448909d492ea353d2137
yuszuv <jan@sternprodukt.de>
2021-10-23 20:16:40 +0200

    add frontend features specs

--- spec/suite/frontend/features/home_spec.rb
+++ spec/suite/frontend/features/home_spec.rb
@@ -1,6 +1,7 @@
 RSpec.feature "Loading the home page", :web do
-  scenario "It shows the page title" do
+  scenario "It show a header and the board" do
     visit "/"
     expect(page).to have_content "Grünes Brett"
+    expect(page).to have_selector "#board"
   end
 end
---
+++ spec/suite/frontend/features/unsubscribe_spec.rb
@@ -0,0 +1,21 @@
+RSpec.feature "Loading the home page", :web, :db do
+  before :each do
+    Factory[:subscriber, confirmation_token: token]
+  end
+
+  let(:token){ SecureRandom.hex(24) }
+
+  context "with a valid token" do
+    scenario "succeeds" do
+      visit "/abonnement_loeschen?token=#{token}"
+      expect(page).to have_content "Automatische Benachrichtigung gelöscht"
+    end
+  end
+
+  context "with an invalid token" do
+    scenario "it return a 404" do
+      visit "/abonnement_loeschen?token=somerandomstuff"
+      expect(page.status_code).to be 404
+    end
+  end
+end
